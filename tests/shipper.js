464cdc59dbcc85e0075d61442dc6f1539eb912d3
Markus Kont <markuskont@gmail.com>
2016-12-29 21:18:23 +0200

    Custom shipper is there. Written badly, but is there

---
+++ tests/shipper.js
@@ -0,0 +1,75 @@
+var kafka = require('kafka-node');
+var moment = require('moment');
+var elasticsearch = require('elasticsearch');
+
+var BULKSIZE = 1024;
+var TYPE = 'events';
+
+// map kafka input topic to ES index and pipeline
+var config = {
+  'apache': {
+    'index': 'syslog',
+    'pipeline': 'apache',
+    'rotate': 'weekly'
+  }
+}
+
+function newTimeValues() {
+  return {
+    'year': moment().year(),
+    'month': moment().month(),
+    'week': moment().week()
+  }
+}
+var rotate = newTimeValues();
+
+var Consumer = kafka.Consumer,
+  client = new kafka.Client(),
+  consumer = new Consumer(
+  client,
+    [
+      {
+        topic: 'apache'
+      }
+    ],
+    {
+      autoCommit: true
+    }
+  );
+
+var client = new elasticsearch.Client({
+  host: 'localhost:9200'
+});
+
+var count = 0;
+  bulk = [];
+
+consumer.on('message', function (message) {
+  count++;
+  var index = config[message.topic]['index'] + '-' + rotate.year;
+  // I assume near-realtime stream, thus current week is good enough (no need to parse rfc3339 timestamp)
+  if (config[message.topic]['rotate'] == 'weekly' ) {
+    index = index + 'w' + rotate.week;
+  } else {
+    index = index + 'm' + rotate.month;
+  } ;
+  var meta = {
+    "index": {
+      '_index': index,
+      '_type': TYPE
+    }
+  };
+  if (config[message.topic]['pipeline']) meta['index']['pipeline'] = config[message.topic]['pipeline'];
+  bulk.push(meta);
+  bulk.push(message.value);
+  if (count % BULKSIZE === 0  ) {
+    // overhead may be low, but I really don't want to compute index timestamp value on every log message
+    rotate = newTimeValues();
+    client.bulk({
+      body: bulk
+    }, function(err, resp) {
+      console.log(err);
+    });
+    bulk = [];
+  }
+});
