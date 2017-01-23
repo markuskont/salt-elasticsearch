7e6ab08741a52c9ee93b5a6bbd2701ba3b452929
Markus Kont <markuskont@gmail.com>
2017-01-23 14:15:06 +0200

    Script improvement. Latest index for easy aggs and timeseries for historical data

--- tests/grain2ela.py
+++ tests/grain2ela.py
@@ -3,7 +3,7 @@
 # apt-get install python-pip
 # pip install elasticsearch
 
-import sys, json, argparse, time
+import sys, json, argparse, time, hashlib
 import salt.client
 from elasticsearch import Elasticsearch
 
@@ -23,26 +23,36 @@ PIPELINE=ARGS.pipeline
 
 SALT_TIMEOUT=60
 
+# these indices will be small. Go for full redundancy
+# Salt grains can contain arrays with different mappings, e.g. ['2', '7', '8', 'final']
+# Not supported by elastic
 TEMPLATE= {
- "order" : 10,
- "version" : 0,
- "template" : "inventory-*",
- "mappings" : {
-   "_default_" : {
-     "properties" : {
-       "pythonversion": {
-         "type": "text",
-         "fields": {
-           "keyword": {
-             "type": "keyword",
-             "ignore_above": 256
-           }
-         }
-       }
-     }
-   }
- },
- "aliases" : { }
+  "order" : 10,
+  "version" : 0,
+  "template" : "inventory-*",
+  "settings" : {
+    "index" : {
+      "refresh_interval" : "60s",
+      "number_of_shards" : 1,
+      "number_of_replicas" : 8
+    }
+  },
+  "mappings" : {
+  "_default_" : {
+    "properties" : {
+      "pythonversion": {
+        "type": "text",
+        "fields": {
+              "keyword": {
+              "type": "keyword",
+              "ignore_above": 256
+            }
+          }
+        }
+      }
+    }
+  },
+  "aliases" : { }
 }
 
 def saltQuery():
@@ -55,29 +65,44 @@ def saltQuery():
 
 def main():
     es = Elasticsearch(hosts=HOSTS, timeout=60)
-    es.indices.create(index=INDEX, ignore=400)
     #es.put_template(id=INDEX, body=TEMPLATE)
 
     timestamp = int(round(time.time()))
 
     grains = saltQuery()
     bulk = []
-    index = "%s-%s" % (INDEX, time.strftime('%Y', time.localtime(timestamp)))
-
-    # migration from puppet to salt, duplicate data
+    timeseries = "%s-%s" % (INDEX, time.strftime('%Y', time.localtime(timestamp)))
+    latest = "%s-latest" % (INDEX)
 
     for host, data in grains.items():
+        # migration from puppet to salt, duplicate data
         if 'facter' in data: del data['facter']
         meta = {
             "index": {
-                "_index": index,
+                "_index": timeseries,
                 "_type": TYPE
             }
         }
         data['@timestamp'] = timestamp * 1000
+
+        # create immutable log of historical data (dynamic ID)
         bulk.append(json.dumps(meta))
         bulk.append(json.dumps(data))
 
+        # create mutable report of current (static ID)
+        # fqdn is already a unique id for saltstack, thus good enough for ES
+        # also use different index
+        meta['index']['_id'] = data['fqdn']
+        meta['index']['_index'] = latest
+
+        bulk.append(json.dumps(meta))
+        bulk.append(json.dumps(data))
+
+    ## Debug
+    # for item in bulk:
+    #     print item
+    
+    # push bulk to ES
     print json.dumps(es.bulk(body=bulk), sort_keys=True, indent=2)
 
 if __name__ == '__main__':
